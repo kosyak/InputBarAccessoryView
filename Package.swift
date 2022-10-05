8debff2737b0ec26f7ffc0c226c657687441f0fb
kosyak <vadim@kosov.eu>
2022-10-05 15:33:34 +0300

    iOS 12 support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "InputBarAccessoryView",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(name: "InputBarAccessoryView", targets: ["InputBarAccessoryView"]),
     ],
